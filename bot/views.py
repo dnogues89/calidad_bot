1dc1010e5f20ceb43d4254cd3925db6a4a959d0a
dnogues89 <damiannogues@gmail.com>
2024-04-19 14:24:07 -0300

    asd

--- bot/views.py
+++ bot/views.py
@@ -58,13 +58,14 @@ class ChatFlow():
             self.cliente.pregunta_1 = self.mensaje
             self.cliente.fecha_finalizacion = datetime.datetime.now()
             Notificaciones(self.cliente).send_card()
-            if self.mensaje != '10' and self.mensaje != '9':
+            if self.mensaje == '10' or self.mensaje == '9':
                 self.cliente.iniciar=True
                 self.cliente.pregunta_1 = self.mensaje
                 self.cliente.fecha_finalizacion = datetime.datetime.now()
-                self.flow = Flow.objects.get(flow_id=102)
+                self.flow = Flow.objects.get(flow_id=101)
                 Notificaciones(self.cliente).send_card()
-                
+            else:
+                self.flow = Flow.objects.get(flow_id=102)
 
 
 
