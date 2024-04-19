b437d0ce4ea50f9e105fd2a67b67a402122e6314
dnogues89 <damiannogues@gmail.com>
2024-04-19 12:48:25 -0300

    asd

--- bot/admin.py
+++ bot/admin.py
@@ -73,7 +73,7 @@ class ClienteAdmin(ImportExportModelAdmin):
                         {
                             "type": "text",
                             "text": obj.entrega
-                        },
+                        }
                     ]
                 }
             ]
